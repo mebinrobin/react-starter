2b60790b026daa7a8ac12fc5ee031c92566c96ba
Mebin Robin <40074239+mebinrobin@users.noreply.github.com>
2019-09-04 00:54:27 -0500

    Add css processing and more changes

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return <Fragment>
             Hello world
-        </Fragment>
+        </Fragment>;
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,99 @@
-const path = require("path");
+const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-const isDev = (process.env.NODE_ENV || '').toLowerCase() != 'production';
+module.exports = (env) => {
+    const isProduction = env.production === true;
+    const isDevelopment = env.development === true;
 
-module.exports = Object.assign({}, {
-    entry: './src/index.js',
-    mode: isDev ? 'development' : 'production',
-    module: {
-        rules: [
-            {
+    return Object.assign({
+        entry: './src/index.js',
+        mode: isDevelopment ? 'development' : isProduction && 'production',
+        module: {
+            rules: [{
                 test: /\.js$/,
                 include: [
-                    path.resolve(__dirname, 'src')
+                    resolve(__dirname, 'src')
                 ],
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 options: {
-                    presets: [
-                        '@babel/env',
-                        '@babel/preset-react'
-                    ]
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }, {
+                test: /\.css$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader
+                },
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        ident: 'postcss',
+                        plugins: () => [
+                            require('postcss-preset-env')()
+                        ]
+                    }
+                }]
+            }]
+        },
+        output: {
+            path: resolve(__dirname, 'build/'),
+            publicPath: './',
+            filename: '[name].[contenthash].js'
+        },
+        optimization: {
+            moduleIds: 'hashed',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all'
+                    }
                 }
             },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'build/'),
-        publicPath: './',
-        filename: '[name].[contenthash].js'
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                  test: /[\\/]node_modules[\\/]/,
-                  name: 'vendors',
-                  chunks: 'all'
-                }
-            }
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin(Object.assign({},
-            {
+            runtimeChunk: true,
+            minimize: isProduction,
+            minimizer: [
+                new TerserPlugin(),
+                new OptimizeCssAssetsPlugin()
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
                 title: 'App',
                 template: 'public/index.html',
-                favicon: 'public/favicon.ico'
-            },
-            !isDev ? {
-                minify: {
+                favicon: 'public/favicon.ico',
+                inlineSource: 'runtime~.+\\.js',
+                minify: isProduction && {
                     collapseWhitespace: true,
                     removeComments: true,
                     useShortDoctype: true
                 }
-            } : undefined
-        ))
-    ],
-    devtool: isDev ? 'source-map' : 'none'
-}, isDev && {
-    devServer: {
-        contentBase: path.resolve(__dirname, 'build/'),
-        port: 3000,
-        publicPath: 'http://localhost:3000/',
-    }
-});
+            }),
+            new ScriptExtHtmlWebpackPlugin({
+                inline: ['runtime'],
+                defaultAttribute: 'defer'
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css',
+                chunkFilename: '[id].[name].[contenthash].css',
+                ignoreOrder: false
+            })
+        ],
+        devtool: isDevelopment ? 'source-map' : 'none',
+        devServer: {
+            port: 3000,
+            publicPath: 'http://localhost:3000/'
+        },
+        stats: {
+            children: false
+        }
+    });
+};
