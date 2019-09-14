dd21466b6e99cb7ae7887507d908ac58aa773050
Mebin Robin <40074239+mebinrobin@users.noreply.github.com>
2019-09-13 20:35:40 -0500

    Format files

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component, Fragment } from 'react';
 
 class App extends Component {
-    render() {
-        return <Fragment>
-            Hello world
-        </Fragment>;
-    }
+  render() {
+    return <Fragment>
+      Hello world
+    </Fragment>;
+  }
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -8,93 +8,93 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env) => {
-    const isProduction = env.production === true;
-    const isDevelopment = env.development === true;
+  const isProduction = env.production === true;
+  const isDevelopment = env.development === true;
 
-    return Object.assign({
-        entry: resolve(__dirname, 'src/index.js'),
-        mode: isDevelopment ? 'development' : isProduction && 'production',
-        module: {
-            rules: [{
-                test: /\.js$/,
-                include: [
-                    resolve(__dirname, 'src')
-                ],
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        '@babel/preset-env',
-                        '@babel/preset-react'
-                    ]
-                }
-            }, {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }]
-        },
-        output: {
-            path: resolve(__dirname, 'build/'),
-            publicPath: './',
-            filename: '[name].[contenthash].js'
-        },
-        optimization: {
-            moduleIds: 'hashed',
-            splitChunks: {
-                cacheGroups: {
-                    vendor: {
-                        test: /node_modules/,
-                        name: 'vendors',
-                        chunks: 'all'
-                    }
-                }
-            },
-            runtimeChunk: true,
-            minimizer: [
-                new TerserPlugin({
-                    extractComments: false
-                }),
-                new OptimizeCssAssetsPlugin()
-            ]
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: resolve(__dirname, 'src/static/index.html'),
-                minify: isProduction && {
-                    collapseWhitespace: true,
-                    removeComments: true,
-                    useShortDoctype: true
-                }
-            }),
-            new ScriptExtHtmlWebpackPlugin({
-                inline: ['runtime'],
-                defaultAttribute: 'defer'
-            }),
-            new MiniCssExtractPlugin({
-                filename: '[name].[contenthash].css',
-                chunkFilename: '[name].[contenthash].chunk.css',
-                ignoreOrder: false
-            }),
-            new CopyPlugin([
-                {
-                    from: resolve(__dirname, 'src/static/'),
-                    to: resolve(__dirname, 'build/'),
-                    ignore: ['index.html']
-                }
-            ])
+  return Object.assign({
+    entry: resolve(__dirname, 'src/index.js'),
+    mode: isDevelopment ? 'development' : isProduction && 'production',
+    module: {
+      rules: [{
+        test: /\.js$/,
+        include: [
+          resolve(__dirname, 'src')
         ],
-        devtool: isDevelopment ? 'inline-source-map' : 'source-map',
-        devServer: {
-            port: 3000,
-            publicPath: 'http://localhost:3000/'
-        },
-        stats: {
-            children: false
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react'
+          ]
         }
-    });
+      }, {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    output: {
+      path: resolve(__dirname, 'build/'),
+      publicPath: './',
+      filename: '[name].[contenthash].js'
+    },
+    optimization: {
+      moduleIds: 'hashed',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /node_modules/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      },
+      runtimeChunk: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false
+        }),
+        new OptimizeCssAssetsPlugin()
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: resolve(__dirname, 'src/static/index.html'),
+        minify: isProduction && {
+          collapseWhitespace: true,
+          removeComments: true,
+          useShortDoctype: true
+        }
+      }),
+      new ScriptExtHtmlWebpackPlugin({
+        inline: ['runtime'],
+        defaultAttribute: 'defer'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[name].[contenthash].chunk.css',
+        ignoreOrder: false
+      }),
+      new CopyPlugin([
+        {
+          from: resolve(__dirname, 'src/static/'),
+          to: resolve(__dirname, 'build/'),
+          ignore: ['index.html']
+        }
+      ])
+    ],
+    devtool: isDevelopment ? 'inline-source-map' : 'source-map',
+    devServer: {
+      port: 3000,
+      publicPath: 'http://localhost:3000/'
+    },
+    stats: {
+      children: false
+    }
+  });
 };
